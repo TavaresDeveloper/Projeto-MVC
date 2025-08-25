341976f8703764bc136731f75da4fa9d705a1d09
TavaresDeveloper <Tavares_23456@outlook.com>
2025-08-24 23:15:04 -0300

    Atualizações

--- config/database.js
+++ config/database.js
@@ -1,10 +1,18 @@
+
+require('dotenv').config();
+
 const sequelize = require('sequelize');
 const { Sequelize } = sequelize;
 
-module.exports = new Sequelize('loja', 'root', '78900',{
-    host: 'localhost',
-    dialect: 'mysql'
 
-});
+module.exports = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
+  }
+);
 
 module.exports.Sequelize = Sequelize;

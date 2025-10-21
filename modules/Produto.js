434411dde6a548a7fb9db98307bc6f74132521e7
TavaresDeveloper <Tavares_23456@outlook.com>
2025-10-21 13:34:16 -0300

    Correções de falha

--- modules/Produto.js
+++ modules/Produto.js
@@ -37,7 +37,7 @@ const Categoria = require('./Categoria');
 
  Produto.belongsTo(Categoria,{
     foreignKey: 'ProdutoCategoria',
-    as: 'categoria'
+    as: 'categorias'
  });
 
 module.exports = Produto;
